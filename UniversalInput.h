52d0a54b58397f9323d3a5d5b8225312814bcdf1
feanor-anglin <44804352+feanor-anglin@users.noreply.github.com>
2018-11-09 13:00:26 +0100

    Update UniversalInput.h

--- UniversalInput.h
+++ UniversalInput.h
@@ -1,9 +1,15 @@
 /*
+ * This is a code written for easy management of digital input/output sensors such as door/window/pir security sensors,
+ * buttons and relays. It menages easy sensor initialization, holds sensor states, changes relays states and provides a quite smart 
+ * function for reading inputs (including special button functionality which change its state after one second of holding a button).
  * 
  * 
  * SensorType:
- * 0 - security sensor (door/window or pir)
- * 1 - button
+ * 0 - Security Sensor (door/window or pir)
+ * 1 - Button Sensor
+ * 2 - Button+Relay sensor
+ * 3 - Relay sensor
+ *
  */
 
 #ifndef UniversalInput_h
@@ -27,23 +33,23 @@
 class UniversalInput
 {
   public:
-	UniversalInput();								                      // SensorType: 0 - security type, 1 - button type, 2 - button+relay type, 3 - relay only
+	UniversalInput();				
 
   int SensorType;
 	int NewState;
 	int OldState;
   
-	void SetValuesS(int SensorPin);								        // Security input type
-	void SetValuesB(int ButtonPin);								        // Button input type
+	void SetValuesS(int SensorPin);						// Security input type
+	void SetValuesB(int ButtonPin);						// Button input type
 	void SetValuesBR(int RelayPin, int ButtonPin);				// Button+Relay input type
-  void SetValuesR(int RelayPin);                        // Message input/Relay output type
+  	void SetValuesR(int RelayPin);                        			// Message input/Relay output type
 	void CheckInput();
 	void SetRelay();
   
   private:
 	int _RelayPin;
 	int _SensorPin;
-  bool _LowStateDetection;
+  	bool _LowStateDetection;
 	bool _HighStateDetection;
 	bool _Condition;
   
